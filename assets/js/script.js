828af609dcfc0f3945ad818c49ec626ddfaff84b
pattytonyoneill <pattytonyoneill@gmail.com>
2021-09-27 14:12:39 +0000

    Add Harry Potter Questions

---
+++ assets/js/script.js
@@ -0,0 +1,337 @@
+function displaypotterQuestion{
+    const questions = [
+        {
+            question: "How many Harry Potter Movies?",
+            optionA: "6",
+            optionB: "8",
+            optionC: "9",
+            optionD: "12",
+            correctOption: "optionB"
+        }
+        {
+            question: "What future Batman actor played Triwizard Tournament entrant Cedric Diggory?",
+            optionA: "Robert Pattinson",
+            optionB: "Christian Bale",
+            optionC: "Ben Affleck",
+            optionD: "Adam West",
+            correctOption: "optionA" 
+        }
+        {
+            question: "Who was Hermione's date at the Yule Ball?",
+            optionA: "Harry Potter",
+            optionB: "Lucious Molfoy",
+            optionC: "Ronald Weasly",
+            optionD: "Viktor Krum",
+            correctOption: "optionD" 
+        }
+        {
+            question: "What was Harry Potter's mom's real name?",
+            optionA: "Rose",
+            optionB: "Tulip",
+            optionC: "Lily",
+            optionD: "Gertrude",
+            correctOption: "optionC" 
+        }
+        {
+            question: "What future Dr Who played Barty Crouch, Jr. in the Goblet of Fire?",
+            optionA: "David Tennant",
+            optionB: "Matt Smith",
+            optionC: "Peter Capaldi",
+            optionD: "Robert Pattinson",
+            correctOption: "optionA" 
+        }
+        {
+            question: "Who was the original actor to play Dumbledore?",
+            optionA: "Michael Gambon",
+            optionB: "David Tennant",
+            optionC: "Robert Pattinson",
+            optionD: "Richard Harris",
+            correctOption: "optionD"  
+        }
+        {
+            question: "What floor was the forbidden corridor in the Philosopher's Stone?",
+            optionA: "3",
+            optionB: "1",
+            optionC: "4",
+            optionD: "2",
+            correctOption: "optionA"  
+        }
+        {
+            question: "What animal represents Hufflepuff house?",
+            optionA: "snake",
+            optionB: "lion",
+            optionC: "badger",
+            optionD: "eagle",
+            correctOption: "optionC"  
+        }
+        {
+            question: "Who was the Minister of Magic for the 1st five movies?",
+            optionA: "Barty Crouch, Sr.",
+            optionB: "Cornelius Fudge",
+            optionC: "Barty Crouch, Jr.",
+            optionD: "Rufus Scrimgeour",
+            correctOption: "optionB"  
+        }
+        {
+            question: "What class has a different teacher each year?",
+            optionA: "Potions",
+            optionB: "Defence of the Dark Arts",
+            optionC: "Divination",
+            optionD: "Charms",
+            correctOption: "optionB"  
+        }
+    ]
+}
+function displayMarvelQuestion { 
+    const questions = [
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+    ]
+}
+function displayDisneyQuestion { 
+    const questions = [
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+    ]
+}
+function displayTrekQuestion { 
+    const questions = [
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+        {
+            question: "?",
+            optionA: "",
+            optionB: "",
+            optionC: "",
+            optionD: "",
+            correctOption: ""
+        }
+    ]
+}
+
