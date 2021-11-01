f3a16ecc576eaa4983a7f3e01d3b540da9b8e3ac
pattytonyoneill <pattytonyoneill@gmail.com>
2021-11-01 10:24:32 +0000

    Edit of quiz.html and js to remove answer box

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,7 @@ function runGame(gameType) {
 
     document.getElementById("potterQuestions");
     
-	document.getElementById("answer-box").value = "";
-	document.getElementById("answer-box").focus();
+	
 
 	if (gameType === "potter") {
 		getPotterQuestions();
