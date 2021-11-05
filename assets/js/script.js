3fcab427ce8734e87d628c97d5f73f823e826371
pattytonyoneill <pattytonyoneill@gmail.com>
2021-11-05 16:09:05 +0000

    Edit of style and attempt to get questions to stop repeating

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,12 +88,15 @@ function incrementWrongAnswer() {
 }
 
 function get_random(list) {
+    while (gameQuestions.length < 0);
+    {
     list_index = Math.floor(Math.random() * list.length);
     current_question = list[list_index];
     gameQuestions.splice(list_index, 1);
     console.log("ARRAY LENGTH, ", list.length);
     return current_question;
     };
+};
 
 //**get quiz questions */
 
